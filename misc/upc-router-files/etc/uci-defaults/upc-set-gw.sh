d8968247663b79b06838f87796e60ec232e3cda4
Pau Escrich <p4u@dabax.net>
2014-02-25 17:53:05 +0100

    Add default DNS for the upc gateway script Add default route via the gre tunnel
    Signed-off-by: Pau Escrich <p4u@dabax.net>

--- misc/upc-router-files/etc/uci-defaults/upc-set-gw.sh
+++ misc/upc-router-files/etc/uci-defaults/upc-set-gw.sh
@@ -3,17 +3,18 @@
 echo "Configuring Wibed gateway"
 
 uci batch << EOF
-set network.upc=route
-set network.upc.target=147.83.0.0
-set network.upc.netmask=255.255.0.0
-set network.upc.gateway=10.8.10.9
-set network.upc.interface=wan
-set network.wan=interface
-set network.wan.ifname=eth0.2
-set network.wan.proto=static
-set network.wan.ipaddr=10.8.10.10
-set network.wan.netmask=255.255.255.248
-set wibed.management.is_gw=1
+set network.loopback.dns="8.8.8.8"
+set network.upc="route"
+set network.upc.target="147.83.0.0"
+set network.upc.netmask="255.255.0.0"
+set network.upc.gateway="10.8.10.9"
+set network.upc.interface="wan"
+set network.wan="interface"
+set network.wan.ifname="eth0.2"
+set network.wan.proto="static"
+set network.wan.ipaddr="10.8.10.10"
+set network.wan.netmask="255.255.255.248"
+set wibed.management.is_gw="1"
 commit
 EOF
 
