fe85f7bb8845428895c95789696f7dcbcaa7030c
pedro-nonfree <pedro-nonfree@users.noreply.github.com>
2015-08-06 21:32:05 +0200

    wibed based on openwrt trunk

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -2,20 +2,24 @@
 
 LINUX_RELEASE?=1
 
-ifeq ($(LINUX_VERSION),3.10.49)
-  LINUX_KERNEL_MD5SUM:=9774e12764e740d49c80eda77d0ef3eb
-endif
+LINUX_VERSION-3.18 = .19
+LINUX_VERSION-4.0 = .9
+LINUX_VERSION-4.1 = .3
 
-# disable the md5sum check for unknown kernel versions
-LINUX_KERNEL_MD5SUM?=x
+LINUX_KERNEL_MD5SUM-3.18.19 = a143f9421bd8413752609d0d771c2955
+LINUX_KERNEL_MD5SUM-4.0.9 = 40fc5f6e2d718e539b45e6601c71985b
+LINUX_KERNEL_MD5SUM-4.1.3 = 96c2c77b1c54ba01cfd8fc2d13fbf828
+
+ifdef KERNEL_PATCHVER
+  LINUX_VERSION:=$(KERNEL_PATCHVER)$(strip $(LINUX_VERSION-$(KERNEL_PATCHVER)))
+endif
 
 split_version=$(subst ., ,$(1))
 merge_version=$(subst $(space),.,$(1))
 KERNEL_BASE=$(firstword $(subst -, ,$(LINUX_VERSION)))
 KERNEL=$(call merge_version,$(wordlist 1,2,$(call split_version,$(KERNEL_BASE))))
-ifeq ($(firstword $(call split_version,$(KERNEL_BASE))),2)
-  KERNEL_PATCHVER=$(call merge_version,$(wordlist 1,3,$(call split_version,$(KERNEL_BASE))))
-else
-  KERNEL_PATCHVER=$(KERNEL)
-endif
+KERNEL_PATCHVER ?= $(KERNEL)
 
+# disable the md5sum check for unknown kernel versions
+LINUX_KERNEL_MD5SUM:=$(LINUX_KERNEL_MD5SUM-$(strip $(LINUX_VERSION)))
+LINUX_KERNEL_MD5SUM?=x
